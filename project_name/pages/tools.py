e6e5f090301b64bb1ecff3a156621320f7907510
Stephen Huang <huangstephen9@gmail.com>
2024-04-20 01:34:47 -0700

    initial commit

---
+++ project_name/pages/tools.py
@@ -0,0 +1,16 @@
+import reflex as rx
+
+from project_name.navigation import navbar
+from project_name.template import template
+
+@template
+def tools() -> rx.Component:
+    return rx.box(
+            navbar(heading="Tools"),
+            rx.box(
+                rx.text("placeholder"),
+                margin_top="calc(50px + 2em)",
+                padding="2em",
+            ),
+            padding_left="250px",
+        )
---
+++ project_name/project_name.py
@@ -0,0 +1,21 @@
+"""The main Dashboard App."""
+
+from rxconfig import config
+
+import reflex as rx
+
+from project_name.styles import BACKGROUND_COLOR, FONT_FAMILY, THEME, STYLESHEETS
+
+from project_name.pages.tools import tools
+from project_name.pages.team import team
+from project_name.pages.index import index
+
+# Create app instance and add index page.
+app = rx.App(
+    theme=THEME,
+    stylesheets=STYLESHEETS,
+)
+
+app.add_page(index, route="/")
+app.add_page(tools, route="/tools")
+app.add_page(team, route="/team")
